4db9399d98b854943db83b49ff528f1def1174ac
yun seo choi <alice30592@gmail.com>
2023-04-20 04:04:45 +0900

    Rename(*.html) html 파일 구조 변경  Chore(package.json) 백엔드에 맞춰 deps 추가  Design(scss) 예제 코드 추가

---
+++ js/fe-main.js
@@ -0,0 +1,7 @@
+// Import the Bootstrap bundle
+//
+// This includes Popper and all of Bootstrap's JS plugins.
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+//
+// Place any custom JS here
+//
