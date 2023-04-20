694edf1689ef6ebbc34770e762b3939f9d6fddfc
merry <merrykang1103@gmail.com>
2023-04-20 18:59:46 +0900

    [Feat] 유저 스키마 몽고디비에 업로드

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const app = express();
 const { Product } = require('./models');
 const { Cart } = require('./models');
 const { Order } = require('./models');
+const { User } = require('./models');
 
 // fake json
 const FakeProduct = require('./models/Fake/f_products.json');
 const FakeCart = require('./models/Fake/f_cart.json');
 const FakeOrder = require('./models/Fake/f_order.json');
+const FakeUser = require('./models/Fake/f_user.json');
 
 // DB 연결
 mongoose
@@ -22,28 +24,33 @@ mongoose
       serverSelectionTimeoutMS: 10000, // 10초
       socketTimeoutMS: 45000, // 45초
       family: 4, // IPv4
-    }
+    },
   )
   .then(() => console.log('Successfully connected to MongoDB'))
-  .catch((err) => console.log(err));
+  .catch(err => console.log(err));
 
 // DB 초기화하는 코드 넣기
 Product.deleteMany({});
 Cart.deleteMany({});
 Order.deleteMany({});
+User.deleteMany({});
 
 // DB 저장
 Product.create(FakeProduct)
-  .then((products) => console.log(products))
-  .catch((err) => console.log(err));
+  .then(products => console.log(products))
+  .catch(err => console.log(err));
 
 Cart.create(FakeCart)
-  .then((cart) => console.log(cart))
-  .catch((err) => console.log(err));
+  .then(cart => console.log(cart))
+  .catch(err => console.log(err));
 
 Order.create(FakeOrder)
-  .then((order) => console.log(order))
-  .catch((err) => console.log(err));
+  .then(order => console.log(order))
+  .catch(err => console.log(err));
+
+User.create(FakeUser)
+  .then(user => console.log(user))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
   res.send('OK');
