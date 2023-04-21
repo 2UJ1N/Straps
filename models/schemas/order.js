fb904b5077f2aa8cc2d6475f5c7d511e518d8b07
HanSeungHyeop <h970409@gmail.com>
2023-04-21 22:18:08 +0900

    [Docs] order.json, order.js에 prod_num 참조태그 추가

--- models/schemas/order.js
+++ models/schemas/order.js
@@ -1,8 +1,9 @@
-const { Schema } = require('mongoose');
+const { Schema } = require("mongoose");
 
 const OrderSchema = new Schema({
   order_num: { type: Number, required: true, unique: true }, // primary key
-  user_id: { type: Schema.Types.ObjectId, ref: 'User' }, // foreign key
+  user_id: { type: Schema.Types.ObjectId, ref: "User" }, // reference
+  prod_num: { type: Schema.Types.ObjectId, ref: "Product" }, // reference
   order_inDate: { type: String, required: true },
 });
 
