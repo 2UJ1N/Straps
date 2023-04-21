fbe175118db49d968a80d3908ac92c8d93404a52
mungseo1012 <tjfls1012@naver.com>
2023-04-21 15:00:07 +0900

    [Fix]cart, order foreign key수정

--- models/schemas/cart.js
+++ models/schemas/cart.js
@@ -1,9 +1,9 @@
-const { Schema } = require("mongoose");
+const { Schema } = require('mongoose');
 
 const CartSchema = new Schema({
   cart_num: { type: Number, required: true, unique: true }, // primary key
-  user_id: { type: Number, required: true },
-  prod_num: { type: Number, required: true },
+  user_id: { type: Schema.Types.ObjectId, ref: 'User' }, // foreign key
+  prod_num: { type: Schema.Types.ObjectId, ref: 'Product' }, // foreign key
   cart_quantity: { type: Number, required: true },
   cart_inDate: { type: String, required: true },
 });
