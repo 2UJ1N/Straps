47ad526fa3c6f008bc63ca04d66a65ec6d28bc51
yun seo choi <ywest1280@gmail.com>
2023-04-25 02:09:02 +0900

    [Feat] user fake api

--- routes-apiSample/user-apiSample.js
+++ routes-apiSample/user-apiSample.js
@@ -15,47 +15,61 @@ router.get('/', (req, res, next) => {
 });
 
 // POST 방식 api
-// /user 
+// /user/login
 // 로그인 api
+router.post('/login', (req, res, next) => {
+  //요청 바디에서 json 객체 가져옴
+  //예시 :
+  // req.body =
+  // {
+  //   "email": "mstubbings0@google.com",
+  //   "password": "s58rBMief",
+  // };
+  //순서 중요함
+  //email, password
+  const { email, password } = req.body;
+  try {
+    // check if the user exists
+    const user = mongooseFake.findOne(email);
+    if (user) {
+      //check if password matches
+      const result = password === user.password;
+      if (result) {
+        let jwt = {
+          jwt : "12uaso1jlsadf"
+        };
+        res.status(200).json(jwt)
+      } else {
+        res.status(400).json({ error: "password doesn't match" });
+      }
+    } else {
+      res.status(400).json({ error: "User doesn't exist" });
+    }
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+
+// POST 방식 api
+// /user/
+// 회원가입 api
 router.post('/', (req, res, next) => {
   //요청 바디에서 json 객체 가져옴
   //예시 :
   // req.body =
   // {
-  //   "email": "mstubbings0@google.com",
-  //   "password": "s58rBMief",
-  // };
-  
-  //순서 중요함
-  //email, password
-  const { email, password } = req.body;
-  const jwt = mongooseFake.create(email, password);
-  res.json(jwt);
-});
-
-// POST 방식 api
-// /user/:user_id 
-// 회원가입 api
-router.post('/:user_id', (req, res, next) => {
-  //요청 바디에서 json 객체 가져옴
-  //예시 :
-  // req.body =
-  // {
-  //   "user_id": 250,
   //   "password": "s58rBMief",
   //   "name": "Molly Stubbings",
   //   "address": "Ulsan",
   //   "phones": "010-6742-6856",
   //   "email": "mstubbings0@google.com",
-  //   "regdate": "12/24/2022",
-  //   "role": false,
-  //   "status": false
+  //   "regdate": "12/24/2022"
   // };
   
   //순서 중요함
-  //user_id, password, name, address, phones, email, regdate, role, status
-  const { user_id, password, name, address, phones, email, regdate, role, status} = req.body;
-  const user = mongooseFake.create(user_id, password, name, address, phones, email, regdate, role, status);
+  //password, name, address, phones, email, regdate
+  const { password, name, address, phones, email, regdate} = req.body;
+  const user = mongooseFake.create(password, name, address, phones, email, regdate);
   res.json(user);
 });
 
