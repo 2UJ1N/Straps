668c48afed1fe8696fcd98707de377eb5134ebf3
yun seo choi <ywest1280@gmail.com>
2023-04-25 01:30:39 +0900

    [Feat] mongo db에 UserSchema 모델 추가

--- models-apiSample/index-apiSample.js
+++ models-apiSample/index-apiSample.js
@@ -2,5 +2,8 @@ const mongoose = require("mongoose");
 
 // 스키마 가져와서 ProductSchema에 할당
 const ProductSchema = require("./schemas-apiSample/product-apiSample");
+const UserSchema = require("./schemas-apiSample/user-apiSample");
+
 
 exports.Product = mongoose.model("Product", ProductSchema);
+exports.User = mongoose.model("User", UserSchema);
