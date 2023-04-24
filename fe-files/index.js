4eb97350e28211d2d3b84556f27b815fa512862b
yun seo choi <ywest1280@gmail.com>
2023-04-24 12:52:42 +0900

    [Test] web component 데모, fetch 모듈화

--- fe-files/index.js
+++ fe-files/index.js
@@ -1,17 +1,154 @@
 //index.html에 사용될 js 스크립트
-async function getProducts() {
-    try {
-      const response = await fetch("http://localhost:3000/products", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json; charset=utf-8",
-        },
+import { getProducts } from './modules/fetchApi.mjs';
+
+const products = getProducts();
+console.log(products);
+// 'use strict';
+
+(function() {
+  class EditableList extends HTMLElement {
+    constructor() {
+      // establish prototype chain
+      super();
+
+      // attaches shadow tree and returns shadow root reference
+      // https://developer.mozilla.org/en-US/docs/Web/API/Element/attachShadow
+      const shadow = this.attachShadow({ mode: 'open' });
+
+      // creating a container for the editable-list component
+      const editableListContainer = document.createElement('div');
+
+      // get attribute values from getters
+      const title = this.title;
+      const addItemText = this.addItemText;
+      const listItems = this.items;
+
+      // adding a class to our container for the sake of clarity
+      editableListContainer.classList.add('editable-list');
+
+      // creating the inner HTML of the editable list element
+      editableListContainer.innerHTML = `
+        <style>
+          li, div > div {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+          }
+          .icon {
+            background-color: #fff;
+            border: none;
+            cursor: pointer;
+            float: right;
+            font-size: 1.8rem;
+          }
+        </style>
+        <h3>${title}</h3>
+        <ul class="item-list">
+          ${listItems.map(item => `
+            <li>${item}
+              <button class="editable-list-remove-item icon">&ominus;</button>
+            </li>
+          `).join('')}
+        </ul>
+        <div>
+          <label>${addItemText}</label>
+          <input class="add-new-list-item-input" type="text"></input>
+          <button class="editable-list-add-item icon">&oplus;</button>
+        </div>
+      `;
+
+      // binding methods
+      this.addListItem = this.addListItem.bind(this);
+      this.handleRemoveItemListeners = this.handleRemoveItemListeners.bind(this);
+      this.removeListItem = this.removeListItem.bind(this);
+
+      // appending the container to the shadow DOM
+      shadow.appendChild(editableListContainer);
+    }
+
+    // add items to the list
+    addListItem(e) {
+      const textInput = this.shadowRoot.querySelector('.add-new-list-item-input');
+
+      if (textInput.value) {
+        const li = document.createElement('li');
+        const button = document.createElement('button');
+        const childrenLength = this.itemList.children.length;
+
+        li.textContent = textInput.value;
+        button.classList.add('editable-list-remove-item', 'icon');
+        button.innerHTML = '&ominus;';
+
+        this.itemList.appendChild(li);
+        this.itemList.children[childrenLength].appendChild(button);
+
+        this.handleRemoveItemListeners([button]);
+
+        textInput.value = '';
+      }
+    }
+
+    // fires after the element has been attached to the DOM
+    connectedCallback() {
+      const removeElementButtons = [...this.shadowRoot.querySelectorAll('.editable-list-remove-item')];
+      const addElementButton = this.shadowRoot.querySelector('.editable-list-add-item');
+
+      this.itemList = this.shadowRoot.querySelector('.item-list');
+
+      this.handleRemoveItemListeners(removeElementButtons);
+      addElementButton.addEventListener('click', this.addListItem, false);
+    }
+
+    // gathering data from element attributes
+    get title() {
+      return this.getAttribute('title') || '';
+    }
+
+    get items() {
+      const items = [];
+
+      [...this.attributes].forEach(attr => {
+        if (attr.name.includes('list-item')) {
+          items.push(attr.value);
+        }
       });
-      const result = await response.json();
-      console.log("Success:", result);
-    } catch (error) {
-      console.error("Error:", error);
+
+      return items;
+    }
+
+    get addItemText() {
+      return this.getAttribute('add-item-text') || '';
+    }
+
+    handleRemoveItemListeners(arrayOfElements) {
+      arrayOfElements.forEach(element => {
+        element.addEventListener('click', this.removeListItem, false);
+      });
+    }
+
+    removeListItem(e) {
+      e.target.parentNode.remove();
     }
   }
-  const products = getProducts();
-  console.log(products);
+
+  // let the browser know about the custom element
+  customElements.define('editable-list', EditableList);
+})();
+
+
+{/* <div class="my-3 p-3 bg-body rounded shadow-sm">
+<div>
+  <div>
+    <img alt="이미지1" data-original="//image.msscdn.net/images/goods_img/20200820/1557508/1557508_4_125.jpg"
+      src="//image.msscdn.net/images/goods_img/20200820/1557508/1557508_4_125.jpg">
+  </div>
+  <p>
+    <a name="goods_link" href="//www.musinsa.com/app/goods/1557508" title="물건1">
+      물건 이름
+    </a>
+  </p>
+  <p>
+    20,700원
+  </p>
+</div>
+</div> */}
