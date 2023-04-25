237316b6cdf2b3bde8469dc13d9c1940f4eb3b47
yun seo choi <ywest1280@gmail.com>
2023-04-25 10:17:46 +0900

    [Chore] vite babel 플러그인 추가

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
-const path = require('path')
+import { defineConfig } from 'vite';
+import babel from 'vite-plugin-babel';
+import path from 'path';
 
-export default {
-  root: path.resolve(__dirname, 'fe-files'),
-  build: {
-    outDir: '../dist'
-  },
-  server: {
-    port: 12345,
-    hot: true
-  }
-}
+export default defineConfig({
+    plugins: [
+        babel(),
+    ],
+    root: path.resolve(__dirname, 'fe-files'),
+    build: {
+      outDirb: '../dist'
+    },
+    server : {
+      port : 3000,
+      hot : true
+    }
+})
