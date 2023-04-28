c38464e547b08039d785b77fe74abf1a23c914b2
HanSeungHyeop <h970409@gmail.com>
2023-04-29 03:36:20 +0900

    cart.js의 reference에 해당하는 user_id, prod_num 타입 수정

--- models/schemas/cart.js
+++ models/schemas/cart.js
@@ -2,8 +2,8 @@ const { Schema } = require("mongoose");
 
 const CartSchema = new Schema({
   cart_num: { type: Number, required: true, unique: true }, // primary key
-  user_id: { type: String, ref: "User" }, // foreign key
-  prod_num: { type: String, ref: "Product" }, // foreign key
+  user_id: { type: Number, ref: "User" }, // reference
+  prod_num: { type: Number, ref: "Product" }, // reference
   cart_quantity: { type: Number, required: true },
   cart_inDate: { type: String, required: true },
 });
