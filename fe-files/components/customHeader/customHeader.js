3ef4affd7a4b04efef6c111ce4f21f96bab96321
yun seo choi <ywest1280@gmail.com>
2023-04-29 10:37:58 +0900

    [Fix] 쇼핑카트 연결

--- fe-files/components/customHeader/customHeader.js
+++ fe-files/components/customHeader/customHeader.js
@@ -1,8 +1,6 @@
 import LoginModal from '../loginModal/loginModal.js';
-import ShoppingCart from '../shoppingCart/shoppingCart.js';
 // 로그인 모달 import
 const loginModal = new LoginModal();
-const shoppingCart = new ShoppingCart();
 
 ////////////////////////////////////////////
 //CustomHeader Component
@@ -43,7 +41,7 @@ export default class CustomHeader extends HTMLElement {
                 <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                 <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
               </svg>
-              <svg id="cartButton" type="button" xmlns="http://www.w3.org/2000/svg" width="32" height="32" class="bi bi-cart-fill justify-content-end" viewBox="0 0 16 16">
+              <svg id="cartButton" type="button" xmlns="http://www.w3.org/2000/svg" width="32" height="32" class="bi bi-cart-fill justify-content-end" viewBox="0 0 16 16" data-bs-toggle="offcanvas" data-bs-target="#cartSideBar">
                 <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
               </svg>
           </div>
@@ -75,7 +73,6 @@ export default class CustomHeader extends HTMLElement {
         elem.addEventListener('click', this.changeAcitveTab, false);
     });
     this.appendChild(loginModal);
-    this.appendChild(shoppingCart);
   }
   update(){
     this.render();
